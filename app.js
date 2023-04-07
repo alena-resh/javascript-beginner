cef0d86680221d0dab8d9112adf91188e361ef94
alena-resh <reshetnikovaalenna@gmail.com>
2023-04-07 16:35:52 +0300

    introduction 2.0 to operators, syntax, statments,

---
+++ app.js
@@ -0,0 +1,67 @@
+// 1. Переменные - хранят значение
+
+// var firstName = 'Alena'
+// const lastName = 'Lim'   // невозможно поменять значение переменной
+// const isProgrammer = true
+// let age = 25   // можно менять значение переменной
+//const age = 25 
+
+// console.log(firstName)
+// console.log(age)
+// console.log(isProgrammer)
+
+// 2. Динамическая типизация - мутирование
+// console.log('Имя человека: ' + firstName + ', а возраст: ' + age)
+
+// const lastName = prompt('Введите фамилию')
+// alert(firstName + ' ' + lastName)
+
+// 3. Операторы JS
+// let currentYear = 2023
+// const birthYear = 1993
+
+// const age = currentYear - birthYear
+// console.log(age)
+
+
+// const a = 10
+// const b = 5
+
+// let c = 32
+// // c = c + a
+// c += a
+// c -= a
+// c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(currentYear++)
+// console.log(currentYear)
+// console.log(c)
+
+// 4. Типы данных
+// const isProgrammer = true
+// const name1 = 'Test'
+// const year = 26
+// let x
+
+// console.log(typeof isProgrammer)
+// console.log(typeof name1)
+// console.log(typeof year)
+// console.log(typeof x) // undefined
+// console.log(typeof null)
+
+// 5. Приоритет операторов
+
+// const fullAge = 25
+// const birthYear = 1993
+// const currentYear = 2023
+
+// // > < >= <=
+// const isFullAge = (currentYear - birthYear) >= fullAge
+// console.log(isFullAge)
+
+
+// 6. Условные операторы
+
