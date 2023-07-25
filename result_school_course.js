c271364bc6bb72896ce448e9febd944c7c387a5e
alena-resh <reshetnikovaalenna@gmail.com>
2023-07-25 12:23:37 +0300

    learning JS - result school

---
+++ result_school_course.js
@@ -0,0 +1,101 @@
+// // Переменные
+
+// let cat = 'Мурзик'
+// cat = 'Барсик'
+
+// console.log(cat)
+
+
+// const dog = 'Шарик'
+// // dog = 'Test'
+
+// console.log(dog)
+
+// // Типы данных
+
+// let string = 'String'
+// let number = 122
+// let boolean = true
+// let object = {
+
+// }
+
+
+// // Математические операторы
+
+// // let result = 10 + 5
+
+// // result = 10 - 5
+
+// // result = 10 / 5
+
+// // console.log(result)
+
+
+// let a = 15
+// let b = 8
+// let c = a + b
+// c = a / b
+
+// console.log(c)
+
+// // Операторы сравнения ===, <, >, <=, >=, !===
+
+// let result = 10 === 5
+// result = 10 !== 5
+// console.log(result)
+
+
+// let a = 15;
+// let b = 25;
+// let c = 35;
+// let d = 45;
+
+// let sum = a + b;
+// let divide = d / a;
+// let mult = c * d;
+// let subtract1 = b - b;
+// let subtract2 = d - b;
+
+// console.log(sum);
+// console.log(divide);
+// console.log(mult);
+// console.log(subtract1);
+// console.log(subtract2);
+
+// let less1 = a < b;
+// console.log(less1);
+
+// let less2 = a < d;
+// console.log(less2);
+
+// let more = a > b;
+// console.log(more);
+
+// let moreEqual = c >= c;
+// console.log(moreEqual);
+
+// let equal = b === d;
+// console.log(equal);
+
+// let equalD = d === d;
+// console.log(equalD);
+
+// let notEqual = c !== a;
+// console.log(notEqual);
+
+// Структуры данных - массив
+
+let array = [1, 'test', true, false]
+// console.log(array[1])
+
+// Объекты
+
+let object = {
+    cat: 'Alice',
+    dog: 'Bob',
+    number: 23,
+    array: [2, 4, 6]
+}
+
+console.log(object.array[1])
